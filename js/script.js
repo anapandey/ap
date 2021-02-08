e23cf3c83ecc37f9eb3551406a30dc1c4ea1a574
Ana <20735577+anapandey@users.noreply.github.com>
2021-02-07 20:07:48 -0800

    Revert "Removing old files"
    This reverts commit 5f455edc6da9ec316cf5c73cbbea5b7efdf0e633.

---
+++ js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function () {
+
+	$(".menu-link").click(function (event) {
+		event.preventDefault();
+		$(".menu-overlay").toggleClass("open");
+		$(".menu").toggleClass("open");
+
+	});
+	//displaying coding skils
+	$(function () {
+		count = 0;
+		skillArray = [
+			"Landing page Design",
+			"WordPress website design based on theme",
+			"WordPress website design from scratch"
+
+		];
+		setInterval(function () {
+			count++;
+			$("#word").fadeOut(1000, function () {
+				$(this).text(skillArray[count % skillArray.length]).fadeIn(1000);
+			});
+		}, 2000);
+	});
+
+	//displaying coding language
+	$(function () {
+		count = 0;
+		languageArray = [
+			"Language used : HTML, CSS, JavaScript and JQuery",
+			"Language used : HTML, CSS, PHP",
+			"Language used : HTML, CSS, PHP"
+		];
+		setInterval(function () {
+			count++;
+			$("#language").fadeOut(1000, function () {
+				$(this).text(languageArray[count % languageArray.length]).fadeIn(1000);
+			});
+		}, 2000);
+	});
+
+	//displaying website features
+	$(function () {
+		count = 0;
+		featureArray = [
+			"Single page website",
+			"Multi page Wordpress website",
+			"Multi page Wordpress website"
+		];
+		setInterval(function () {
+			count++;
+			$("#features").fadeOut(1000, function () {
+				$(this).text(featureArray[count % featureArray.length]).fadeIn(1000);
+			});
+		}, 2000);
+	});
+
+	//displaying website type
+	$(function () {
+		count = 0;
+		typeArray = [
+			"Static website",
+			"Dynamic website with CMS",
+			"Dynamic website with CMS"
+		];
+		setInterval(function () {
+			count++;
+			$("#type").fadeOut(1000, function () {
+				$(this).text(typeArray[count % typeArray.length]).fadeIn(1000);
+			});
+		}, 2000);
+	});
+
+});
